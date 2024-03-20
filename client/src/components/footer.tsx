cc07fa708da29c6b0a884e488921a1eca5189d17
cha777 <chathuranga_wm@yahoo.com>
2024-03-20 10:10:45 +0530

    Included client app version in the web page

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -1,7 +1,12 @@
 import {} from 'react-bootstrap';
 
 const Footer = () => {
-  return <footer className='my-5 pt-5 text-muted text-center text-small'>© {new Date().getFullYear()} DirectFN</footer>;
+  return (
+    <footer className='my-5 pt-5 text-muted text-center text-small'>
+      © {new Date().getFullYear()} DirectFN <br />
+      <small>v{import.meta.env.PACKAGE_VERSION}</small>
+    </footer>
+  );
 };
 
 export default Footer;
