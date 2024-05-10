371bf87b9e18b18a254360cffc0897613eb68e70
cha777 <chathuranga_wm@yahoo.com>
2024-05-10 08:56:42 +0530

    Included file server version in logs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const logger = require('./logger').default;
+const packageJson = require('../package.json');
 
+logger.info(`File server version: ${packageJson.version}`);
 logger.info('Configuring server');
 
 require('dotenv').config({ path: path.resolve('.env') });
